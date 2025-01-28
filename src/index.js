cfe382e80dff3edead1cf239c66a2ea4dbe1d733
Hyper <hypersoweak@gmail.com>
2025-01-29 01:04:40 +0800

    feat: event system

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { readdirSync } from 'node:fs';
-import { join, dirname } from 'node:path';
+import fs from 'node:fs';
+import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { Client, Collection, Events, GatewayIntentBits, MessageFlags } from 'discord.js';
 import config from './config.json' with { 'type': 'json' };
@@ -7,17 +7,18 @@ import config from './config.json' with { 'type': 'json' };
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 client.commands = new Collection();
-const foldersPath = join(__dirname, 'commands');
-const commandFolders = readdirSync(foldersPath);
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-  const commandsPath = join(foldersPath, folder);
-  const commandFiles = readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
   for (const file of commandFiles) {
-    const filePath = join(commandsPath, file);
+    const filePath = path.join(commandsPath, file);
     const command = await import(filePath);
     if ('data' in command && 'execute' in command) {
       client.commands.set(command.data.name, command);
@@ -27,35 +28,17 @@ for (const folder of commandFolders) {
   }
 }
 
-client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: 'There was an error while executing this command!',
-        flags: MessageFlags.Ephemeral,
-      });
-    } else {
-      await interaction.reply({
-        content: 'There was an error while executing this command!',
-        flags: MessageFlags.Ephemeral,
-      });
-    }
-  }
-});
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = await import(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 client.login(config.client.token);
