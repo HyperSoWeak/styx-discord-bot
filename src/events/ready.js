bf54336b0214f01fc0b5578759129669ff00f421
Hyper <hypersoweak@gmail.com>
2025-01-30 15:17:05 +0800

    feat: setPresence

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,4 @@
-import { Events } from 'discord.js';
+import { Events, ActivityType } from 'discord.js';
 import { connectDB } from '../utils/database.js';
 
 export const name = Events.ClientReady;
@@ -7,4 +7,9 @@ export const once = true;
 export function execute(client) {
   console.log(`Ready! Logged in as ${client.user.tag}`);
   connectDB();
+
+  client.user.setPresence({
+    activities: [{ name: '你媽', type: ActivityType.Playing }],
+    status: 'online',
+  });
 }
