32477ef4b80c088b2563d611343037b4392888a3
Hyper <hypersoweak@gmail.com>
2025-01-30 21:26:10 +0800

    feat: modify msgCount system - add haoshuangCount

--- src/events/messaageCreate.js
+++ src/events/messaageCreate.js
@@ -1,6 +1,7 @@
 import { Events } from 'discord.js';
 import MsgCount from '../models/msgCount.js';
 import { getGuildSettings } from '../utils/getter.js';
+import msgCountList from '../data/msgCount.js';
 
 export const name = Events.MessageCreate;
 
@@ -9,22 +10,26 @@ export async function execute(message) {
 
   const guildSettings = await getGuildSettings(message.guild.id);
 
+  // message count feature
   if (guildSettings.hasMsgCount) {
-    if (message.content.includes('破防') || message.content.includes('破大防')) {
-      try {
-        let userMsgCount = await MsgCount.findOne({ userId: message.author.id });
+    for (const { keywords, responses, counter } of msgCountList) {
+      if (keywords.some((keyword) => message.content.includes(keyword))) {
+        try {
+          let userMsgCount = await MsgCount.findOne({ userId: message.author.id });
 
-        if (!userMsgCount) {
-          userMsgCount = new MsgCount({ userId: message.author.id, pofangCount: 1 });
-          await userMsgCount.save();
-        } else {
-          userMsgCount.pofangCount++;
-          await userMsgCount.save();
+          if (!userMsgCount) {
+            userMsgCount = new MsgCount({ userId: message.author.id, [counter]: 1 });
+            await userMsgCount.save();
+          } else {
+            userMsgCount[counter]++;
+            await userMsgCount.save();
+          }
+
+          const response = responses(message, userMsgCount, counter);
+          await message.reply(response);
+        } catch (err) {
+          console.error('Error while updating user count:', err);
         }
-
-        await message.reply(`${message.author.username}，你已經破防 ${userMsgCount.pofangCount} 次了 QwQ`);
-      } catch (err) {
-        console.error('Error while updating user count:', err);
       }
     }
   }
--- src/models/msgCount.js
+++ src/models/msgCount.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const msgCountSchema = new mongoose.Schema({
   userId: { type: String, required: true, unique: true },
   pofangCount: { type: Number, default: 0 },
+  haoshuangCount: { type: Number, default: 0 },
 });
 
 const MsgCount = mongoose.model('MsgCount', msgCountSchema);
